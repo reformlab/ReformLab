cea52445af4d046b42a67669888dabee6b990877
Lucas Vivier <79191733+lucas-vivier@users.noreply.github.com>
2026-03-06 17:40:02 +0100

    Update shcemas.pyi

--- src/reformlab/data/schemas.pyi
+++ src/reformlab/data/schemas.pyi
@@ -2,7 +2,6 @@ import pyarrow as pa
 
 from reformlab.computation.ingestion import DataSchema
 
-
 def fill_missing_energy_columns(table: pa.Table) -> pa.Table: ...
 
 SYNTHETIC_POPULATION_SCHEMA: DataSchema
